e32fe6dc02a616720afac53f33cf7815f621f6ef
Alex <s8309200@yandex.ru>
2025-04-21 15:23:08 +0400

    Настроить проект

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
+const Div = styled.div`
+	text-align: center;
+`;
 const App = () => {
 	return (
-		<>
+		<Div>
 			<div>App</div>
 			<i className="fa fa-camera-retro"></i>
-		</>
+		</Div>
 	);
 };
 
