050cca608d099a70315d47210e1e12adbbaad7f2
Alex <s8309200@yandex.ru>
2025-04-29 10:33:44 +0400

    Реализовать шапку

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,19 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
 import styled from 'styled-components';
 
-const Content = styled.content`
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	margin: 0 auto;
+	background-color: #fff;
+`;
+
+const Content = styled.div`
 	padding: 120px 0;
 `;
 
 const H2 = styled.h2`
 	text-align: center;
 `;
-const Header = () => <div>Шапка</div>;
+
 const Footer = () => <div>Подвал</div>;
 
 const Blog = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Контент страницы</H2>
@@ -28,7 +39,7 @@ const Blog = () => {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
 
